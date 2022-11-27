7ae9c676df1108e71408837b3eee26ce36efa55f
Santiago <55521570+SantiagoLondono8@users.noreply.github.com>
2022-11-27 00:13:27 -0500

    Add files via upload

---
+++ Proyecto Final/Contacto.java
@@ -0,0 +1,10 @@
+package contacto;
+import javax.swing.JFrame;
+public class Contacto {
+    public static void main(String[] args) {
+        Interfaz principal=new Interfaz();
+        principal.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        principal.setVisible(true);
+    }
+    
+}
